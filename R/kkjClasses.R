3352867c2aa843d138e9e69ba97a52716d923c75
Peter Solymos <psolymos@gmail.com>
2016-12-04 00:24:58 -0700

    reorg

--- R/kkjClasses.R
+++ R/kkjClasses.R
@@ -1,11 +1,11 @@
 ## class for jokes
-setClass("KnockKnockJoke", 
+setClass("KnockKnockJoke",
     representation(
         name = "character",
         punch = "character"),
     prototype = list(
         name = character(0),
-        punchline = character(0)))
+        punch = character(0)))
 
 ## creator function for jokes
 KnockKnockJoke <- function(name, punch) {
@@ -27,15 +27,3 @@ setMethod("show", "KnockKnockJoke", function(object) {
         fill=getOption("width")-5)
     invisible(object)
 })
-
-## function to access jokes
-KnockKnock <- function(x, data) {
-  if (missing(data)) {
-    e <- new.env()
-    utils:::data(kkjokes, envir=e)
-    data <- get("kkjokes", envir=e)
-  }
-  if (missing(x))
-    x <- sample.int(length(data), 1)
-  data[[x[1]]]
-}
